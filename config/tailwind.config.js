9717169d01ce2297b99bc1a8d73e6b930acf6f71
Jose A. Leiva <jleivaizq@gmail.com>
2023-09-17 13:59:33 +0200

    Adds a minimal navigation and themes from Daisy examples

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -24,6 +24,36 @@ module.exports = {
     require("daisyui")
   ],
   daisyui: {
-    themes: ["light", "dark", "cupcake"],
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter'
+    ]
   }
 }
